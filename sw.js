fce1036669e0d68a6f40e8150a5fa3fe430c41b1
overanti-lab <shie0976514976@gmail.com>
2026-05-23 14:44:26 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = 'reaction-game-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = [
+  '/reaction-game/',
+  '/reaction-game/index.html',
+  '/reaction-game/manifest.json',
+  '/reaction-game/icon-192.png',
+  '/reaction-game/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
